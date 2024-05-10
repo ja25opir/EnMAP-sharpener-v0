59c59493af3ebbdb6c6cb277d9bfaf0b4646d464
Marvin Müller <marvin.mueller@singular-it.de>
2024-05-10 10:47:27 +0200

    add zscore calc for file list

---
+++ src/zscore_calc.py
@@ -0,0 +1,21 @@
+import os
+from scipy import stats
+from matplotlib import pyplot as plt
+
+
+sentinel_dir = os.getcwd() + '/data/preprocessing/Sentinel2/'
+file_list = os.listdir(sentinel_dir)
+
+size_list = []
+for file in file_list:
+    size_list.append(os.stat(sentinel_dir + file).st_size)
+
+zscore = stats.zscore(size_list)
+standard_deviation = stats.tstd(size_list)
+for i in range(len(zscore)):
+    if zscore[i] < 0:
+        print(file_list[i], size_list[i])
+# plt.hist(size_list, bins=100, density=False, alpha=0.75, color='b')
+# plt.show()
+# plt.hist(zscore, bins=100, density=False, alpha=0.75, color='r')
+# plt.show()
