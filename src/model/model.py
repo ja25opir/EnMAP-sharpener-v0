51bad098ac429a08d33c9f9895c484917d4d9f72
Marvin Müller <marvin.mueller@singular-it.de>
2024-05-17 09:24:42 +0200

    update model

--- src/model/model.py
+++ src/model/model.py
@@ -79,8 +79,8 @@ class Model:
     def train_test_split(self):
         all_files = os.listdir(self.train_data_dir + 'x/')
         # todo: shuffle? -> in DataGenerator
-        self.train_files = all_files[:int(len(all_files) * 0.4)] # todo: WIP
-        self.test_files = all_files[int(len(all_files) * 0.1):]
+        self.train_files = all_files[:int(len(all_files) * 0.3)] # todo: WIP
+        self.test_files = all_files[int(len(all_files) * 0.9):]
         print('Train data size:', len(self.train_files))
         print('Test data size:', len(self.test_files))
 
