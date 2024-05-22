8969f83357e348667bdff929e154c6bca7d5694e
Marvin Müller <marvin.mueller@singular-it.de>
2024-05-22 14:42:07 +0200

    update model

--- src/model/model.py
+++ src/model/model.py
@@ -39,20 +39,40 @@ class Model:
         # padding: https://hidayatullahhaider.medium.com/a-simple-definition-of-overlap-term-in-cnn-f331f6ef3031
         # padding: https://openreview.net/pdf?id=M4qXqdw3xC#:~:text=Recent%20studies%20have%20shown%20that,of%20padding%20precludes%20position%20encoding
         model = models.Sequential()
-        model.add(layers.Conv2D(1024,
-                                self.kernel_size_list[0],
-                                activation='relu',
-                                input_shape=(self.tile_size, self.tile_size, self.no_input_bands),
-                                padding='same'))
-        # second hidden layer with more neurons worsens loss and accuracy (reason: lack of data maybe)
-        model.add(layers.Conv2D(512,
-                                self.kernel_size_list[1],
-                                activation='relu',
-                                padding='same'))
-        # model.add(layers.Conv2D(300,
+        # model.add(layers.Conv2D(1024,
+        #                         self.kernel_size_list[0],
+        #                         activation='relu',
+        #                         input_shape=(self.tile_size, self.tile_size, self.no_input_bands),
+        #                         padding='same'))
+        # # second hidden layer with more neurons worsens loss and accuracy (reason: lack of data maybe)
+        # model.add(layers.Conv2D(512,
         #                         self.kernel_size_list[1],
         #                         activation='relu',
         #                         padding='same'))
+        model.add(layers.Conv2D(self.no_output_bands,
+                                self.kernel_size_list[0],
+                                activation='relu',
+                                padding='same'))
+        model.add(layers.Conv2D(self.no_output_bands,
+                                self.kernel_size_list[1],
+                                activation='relu',
+                                padding='same'))
+        model.add(layers.Conv2D(self.no_output_bands,
+                                self.kernel_size_list[1],
+                                activation='relu',
+                                padding='same'))
+        model.add(layers.Conv2D(self.no_output_bands,
+                                self.kernel_size_list[1],
+                                activation='relu',
+                                padding='same'))
+        model.add(layers.Conv2D(self.no_output_bands,
+                                self.kernel_size_list[1],
+                                activation='relu',
+                                padding='same'))
+        model.add(layers.Conv2D(self.no_output_bands,
+                                self.kernel_size_list[1],
+                                activation='relu',
+                                padding='same'))
         # model.add(layers.Conv2D(512,
         #                         self.kernel_size_list[1],
         #                         activation='relu',
@@ -79,8 +99,8 @@ class Model:
     def train_test_split(self):
         all_files = os.listdir(self.train_data_dir + 'x/')
         # todo: shuffle? -> in DataGenerator
-        self.train_files = all_files[:int(len(all_files) * 0.8)] # todo: WIP
-        self.test_files = all_files[int(len(all_files) * 0.8):]
+        self.train_files = all_files[:int(len(all_files) * 0.1)] # todo: WIP
+        self.test_files = all_files[int(len(all_files) * 0.9):]
         print('Train data size:', len(self.train_files))
         print('Test data size:', len(self.test_files))
 
--- src/model/test_model.py
+++ src/model/test_model.py
@@ -2,6 +2,7 @@ import os, random
 import rasterio
 import tensorflow as tf
 import numpy as np
+import keras.backend as K
 
 from src.visualization.helpers import get_bands_from_array
 from src.visualization.plot_raster import plot_3_band_image
@@ -23,7 +24,8 @@ model = tf.keras.models.load_model(model_path + 'first_model.keras')
 
 print(model.summary())
 
-predicted_raster = model.predict(x_raster.T.reshape(1, 100, 100, 228)).reshape(100, 100, 224).T
+model_input = x_raster.T.reshape(1, 100, 100, 228)
+predicted_raster = model.predict(model_input).reshape(100, 100, 224).T
 
 bands = [50, 100, 150]
 predicted_rgb = get_bands_from_array(predicted_raster, bands)
@@ -35,4 +37,14 @@ plot_3_band_image(x_rgb, title='Input Image')
 y_rgb = get_bands_from_array(y_raster, bands)
 plot_3_band_image(y_rgb, title='Original Image')
 
+for i in range(len(model.layers)):
+    get_layer_output = K.function(inputs = model.layers[0].input, outputs = model.layers[i].output)
+
+    get_1_output = get_layer_output(model_input)
+    # print(get_1_output.shape) << Use this to check if the Output shape matches the shape of Model.summary()
+
+    arr = get_bands_from_array(get_1_output[0].T, bands)
+    plot_3_band_image(arr, title='Layer ' + str(i + 1))
+
+# currently layer 1 and 2 only display clouds / noise (no real features)
 # todo: for a real prediction we have to upscale an enmap image that was not used for training, and add sentinel bands
