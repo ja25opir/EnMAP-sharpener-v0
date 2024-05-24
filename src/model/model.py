910c945ed2babb1b3510d2855562a811b0dba636
Marvin Müller <marvin.mueller@singular-it.de>
2024-05-24 12:40:30 +0200

    fix model

--- src/model/model.py
+++ src/model/model.py
@@ -109,19 +109,19 @@ class Model:
         x = model.add(layers.Conv2D(64,
                                     self.kernel_size_list[0],
                                     activation=tf.keras.layers.LeakyReLU(alpha=0.01),
-                                    padding='valid'))(x)
+                                    padding='valid')(x))
         for i in range(10):
             x = SymmetricPadding2D(padding=(1, 1))(x)
             model.add(layers.Conv2D(64,
                                     self.kernel_size_list[1],
                                     activation=tf.keras.layers.LeakyReLU(alpha=0.01),
                                     kernel_regularizer=regularizers.l1(0.001),
-                                    padding='valid'))(x)
+                                    padding='valid')(x))
         x = SymmetricPadding2D(padding=(1, 1))(x)
         model.add(layers.Conv2D(self.no_output_bands,
                                 (5, 5),
                                 activation='linear',
-                                padding='linear'))(x)
+                                padding='linear')(x))
 
         # Masi
         # model.add(layers.Conv2D(64,
