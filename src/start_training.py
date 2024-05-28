0d85189f182e1cd3af26787cff7b131a74cc7bce
Marvin Müller <marvin.mueller@singular-it.de>
2024-05-28 13:46:17 +0200

    update gpu limiter

--- src/start_training.py
+++ src/start_training.py
@@ -35,12 +35,15 @@ if __name__ == '__main__':
     # flexible_tf_gpu_memory_growth(args.gpus)
 
     import tensorflow as tf
-    tf.debugging.set_log_device_placement(True)
+
+    # tf.debugging.set_log_device_placement(True)
     gpus = tf.config.list_logical_devices('GPU')
-    strategy = tf.distribute.MirroredStrategy([gpus[1], gpus[2]])
-    with strategy.scope():
-        cnn_model = Model(TRAIN_DATA_DIR, TILE_SIZE, NO_INPUT_BANDS, NO_OUTPUT_BANDS, BATCH_SIZE, KERNEL_SIZES,
-                          LOSS_FUNCTION, TRAIN_EPOCHS, OUTPUT_DIR)
+    for gpu_arg in args.gpus:
+        gpu = gpus[gpu_arg]
+        print('Using GPU', gpu.name)
+        with tf.device(gpu.name):
+            cnn_model = Model(TRAIN_DATA_DIR, TILE_SIZE, NO_INPUT_BANDS, NO_OUTPUT_BANDS, BATCH_SIZE, KERNEL_SIZES,
+                              LOSS_FUNCTION, TRAIN_EPOCHS, OUTPUT_DIR)
 
     print('Starting training...')
     cnn_model.train_model()
