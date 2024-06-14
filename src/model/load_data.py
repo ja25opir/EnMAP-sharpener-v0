2bcfc30eddc547bf611ce283bb55231304b9457c
Marvin Müller <marvin.mueller@singular-it.de>
2024-06-14 15:50:58 +0200

    fix dataloader (finally...)

--- src/model/load_data.py
+++ src/model/load_data.py
@@ -87,7 +87,7 @@ class DuoBranchDataGenerator(Sequence):
         # (batch_size, w, h, no_input_bands)
         X = np.empty((self.batch_size, *self.output_size, self.no_input_bands))
         X1 = np.empty((self.batch_size, *self.output_size, self.no_output_bands))
-        Y = np.empty((self.batch_size, *self.output_size, self.no_output_bands))
+        out = np.empty((self.batch_size, *self.output_size, self.no_output_bands))
 
         # get the indices of the requested batch
         indices = self.indices[idx * self.batch_size:(idx + 1) * self.batch_size]
@@ -107,10 +107,10 @@ class DuoBranchDataGenerator(Sequence):
             # transpose img as model expects (w, h, no_bands) and img has shape (no_bands, h, w)
             X[i,] = x_img.T
             X1[i,] = x1_img.T
-            Y[i,] = y_img.T
+            out[i,] = y_img.T
 
         # return [X, X1], [Y]
-        return {'x': X, 'x1': X1}, Y
+        return {'x': X, 'x1': X1}, out
 
 
 # class DuoBranchDataGenerator(DataGenerator):
