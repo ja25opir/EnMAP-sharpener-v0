40b7f3aa3ee0bfa4727fa71168e3699a82a4f7f3
Marvin Müller <marvin.mueller@singular-it.de>
2024-06-17 11:23:28 +0200

    update Test3dConv

--- src/model/model.py
+++ src/model/model.py
@@ -63,8 +63,8 @@ class Model:
     def train_test_split(self):
         all_files = os.listdir(self.train_data_dir + 'x/')
         # todo: shuffle? -> in DataGenerator
-        self.train_files = all_files[:int(len(all_files) * 0.1)]  # todo: WIP
-        self.test_files = all_files[int(len(all_files) * 0.9):]
+        self.train_files = all_files[:int(len(all_files) * 0.01)]  # todo: WIP
+        self.test_files = all_files[int(len(all_files) * 0.09):]
         print('Train data size:', len(self.train_files))
         print('Test data size:', len(self.test_files))
 
