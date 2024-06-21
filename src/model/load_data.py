99a011c67b14b47fdbb5426d96d268e9b8109d4e
Marvin Müller <marvin.mueller@singular-it.de>
2024-06-21 12:29:52 +0200

    use TestSaPNN with 24 + 4 bands

--- src/model/load_data.py
+++ src/model/load_data.py
@@ -86,8 +86,8 @@ class DuoBranchDataGenerator(DataGenerator):
             # x_img = x_img[(50, 100, 150, 225, 226, 227), :, :]
             # x1_img = x1_img[(50,100,150), :, :]
             # y_img = y_img[(50,100,150), :, :]
-            x_img = x_img[80:100, :, :]
-            x1_img = np.hstack([x1_img[80:100, :, :], x_img[224:, :, :]])
+            x_img = np.hstack([x_img[80:100, :, :], x_img[224:, :, :]])
+            x1_img = x1_img[80:100, :, :]
             y_img = y_img[80:100, :, :]
 
             # transpose img as model expects (w, h, no_bands) and img has shape (no_bands, h, w)
