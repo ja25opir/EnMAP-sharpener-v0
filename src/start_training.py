5d0034ddb77acfe07c99f763b717408f2ff248aa
Marvin Müller <marvin.mueller@singular-it.de>
2024-06-24 13:52:37 +0200

    add epochs and train dir as cli args

--- src/start_training.py
+++ src/start_training.py
@@ -12,18 +12,16 @@ NO_INPUT_BANDS = 3 + 3
 NO_OUTPUT_BANDS = 3
 KERNEL_SIZES = [(9, 9), (3, 3), (5, 5)]
 
-TRAIN_DATA_DIR = os.getcwd() + '/data/preprocessing/model_input/'
 OUTPUT_DIR = os.getcwd() + '/output/'
 LOSS_FUNCTION = 'mean_squared_error'  # todo: adapt learn rate and momentum, also use other loss function
 # LEARN_RATE = 0.00001
 # https://www.activeloop.ai/resources/glossary/adaptive-learning-rate-methods/#:~:text=Adaptive%20learning%20rate%20methods%20improve%20deep%20learning%20model%20performance%20by,faster%20convergence%20and%20better%20generalization.
 # https://stats.stackexchange.com/questions/383807/why-we-call-adam-an-a-adaptive-learning-rate-algorithm-if-the-step-size-is-a-con
-TRAIN_EPOCHS = 10
 
 @multiple_gpu_distribution
-def train_model(batch_size):
-    cnn_model = Model(TRAIN_DATA_DIR, TILE_SIZE, NO_INPUT_BANDS, NO_OUTPUT_BANDS, batch_size, KERNEL_SIZES,
-                      LOSS_FUNCTION, TRAIN_EPOCHS, OUTPUT_DIR)
+def train_model(batch_size, epochs, train_data):
+    cnn_model = Model(train_data, TILE_SIZE, NO_INPUT_BANDS, NO_OUTPUT_BANDS, batch_size, KERNEL_SIZES,
+                      LOSS_FUNCTION, epochs, OUTPUT_DIR)
 
     print('Starting training...')
     cnn_model.train_model()
@@ -36,18 +34,21 @@ if __name__ == '__main__':
                         help='Assigned GPUs for the pipeline')
     parser.add_argument('--mem-limit', type=int, default=10, help='GPU memory limit training in GB (per GPU)')
     parser.add_argument('--batch-size', type=int, default=32, help='Batch size for training')
+    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs for training')
+    parser.add_argument('--train-data-dir', type=str, default='/data/preprocessing/model_input/', help='Path to the directory with training data')
 
     args = parser.parse_args()
 
-    if os.path.exists(TRAIN_DATA_DIR + 'x/.gitkeep'):
-        os.remove(TRAIN_DATA_DIR + 'x/.gitkeep')
-    if os.path.exists(TRAIN_DATA_DIR + 'y/.gitkeep'):
-        os.remove(TRAIN_DATA_DIR + 'y/.gitkeep')
+    train_data_dir = os.getcwd() + args.train_data_dir
+    if os.path.exists(train_data_dir + 'x/.gitkeep'):
+        os.remove(train_data_dir + 'x/.gitkeep')
+    if os.path.exists(train_data_dir + 'y/.gitkeep'):
+        os.remove(train_data_dir + 'y/.gitkeep')
 
     limit_gpu_memory_usage(args.gpus, args.mem_limit)
 
     start = time.time()
-    model = train_model(args.batch_size)
+    model = train_model(args.batch_size, args.epochs, train_data_dir)
     end = time.time()
 
     model.plot_history()
