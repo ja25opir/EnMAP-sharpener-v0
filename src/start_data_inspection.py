42630582b57b16185e48d4061ed7326f48f12752
Marvin Müller <marvin.mueller@singular-it.de>
2024-07-22 14:59:30 +0200

    fix(data): deactivate data plotting for knossos

--- src/start_data_inspection.py
+++ src/start_data_inspection.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 from data.find_outlier import copy_outliers
-from visualization.plot_outlier import plot_size_histogram, plot_corresponding_scenes
+# from visualization.plot_outlier import plot_size_histogram, plot_corresponding_scenes
 
 SENTINEL_DIR = os.getcwd() + '/data/preprocessing/Sentinel2/'
 ENMAP_DIR = os.getcwd() + '/data/preprocessing/EnMAP/'
@@ -18,14 +18,14 @@ if __name__ == '__main__':
     enmap_figures_dir = os.getcwd() + '/output/figures/broken_files/EnMAP/'
     copy_outliers(ENMAP_DIR, SENTINEL_DIR, enmap_outlier_dir, enmap_figures_dir, corr_suffix='_sentinel_spectral.tif')
 
-    # plot Sentinel outlier
-    sentinel_size_df = pd.read_pickle(sentinel_figures_dir + '/file_size_df.pkl')
-    sentinel_size_df = sentinel_size_df[sentinel_size_df['file'].str.contains('_spectral.tif')]
-    plot_size_histogram(sentinel_size_df, sentinel_figures_dir, quant=0.05)
-    plot_corresponding_scenes(sentinel_outlier_dir, output_dir=sentinel_figures_dir)
-
-    # plot EnMAP outlier
-    enmap_size_df = pd.read_pickle(enmap_figures_dir + 'file_size_df.pkl')
-    enmap_size_df = enmap_size_df[enmap_size_df['file'].str.contains('_spectral.tif')]
-    plot_size_histogram(enmap_size_df, enmap_figures_dir, quant=0.04)
-    plot_corresponding_scenes(enmap_outlier_dir, output_dir=enmap_figures_dir, outlier='enmap', corresponding='sentinel')
+    # # plot Sentinel outlier
+    # sentinel_size_df = pd.read_pickle(sentinel_figures_dir + '/file_size_df.pkl')
+    # sentinel_size_df = sentinel_size_df[sentinel_size_df['file'].str.contains('_spectral.tif')]
+    # plot_size_histogram(sentinel_size_df, sentinel_figures_dir, quant=0.05)
+    # plot_corresponding_scenes(sentinel_outlier_dir, output_dir=sentinel_figures_dir)
+    #
+    # # plot EnMAP outlier
+    # enmap_size_df = pd.read_pickle(enmap_figures_dir + 'file_size_df.pkl')
+    # enmap_size_df = enmap_size_df[enmap_size_df['file'].str.contains('_spectral.tif')]
+    # plot_size_histogram(enmap_size_df, enmap_figures_dir, quant=0.04)
+    # plot_corresponding_scenes(enmap_outlier_dir, output_dir=enmap_figures_dir, outlier='enmap', corresponding='sentinel')
