5855c3d7ab838b033fa7f761a2401b923807793c
Marvin Müller <marvin.mueller@singular-it.de>
2024-07-26 12:19:32 +0200

    MMSRes: add edges1 twice

--- src/model/architecture.py
+++ src/model/architecture.py
@@ -375,8 +375,8 @@ class MMSRes:
         # main branch
         input3d = Input(shape=(self.tile_size, self.tile_size, self.no_output_bands, 1), name='x1')
         conv1 = layers.Conv3D(64, (9, 9, 7), padding='same', activation=leakyRelu)(input3d)
+        edges1 = tf.concat([edges1, edges1], axis=-1)
         merged1 = DILayer()([conv1, edges1])
-        merged1 = DILayer()([edges1, merged1])
 
         skip_connection = layers.Add()([input3d, merged1])
 
