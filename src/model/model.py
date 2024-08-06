622da09366b25322618088b4b6acdd8eb0e7777b
Marvin Müller <marvin.mueller@singular-it.de>
2024-08-06 22:26:37 +0200

    MMSRes: lower learn rate

--- src/model/model.py
+++ src/model/model.py
@@ -76,7 +76,7 @@ class Model:
         print('Test data size:', len(self.test_files))
 
     def set_lr_schedule(self):
-        initial_learning_rate = 0.001
+        initial_learning_rate = 0.0001
         final_learning_rate = 0.00001
         learning_rate_decay_factor = (final_learning_rate / initial_learning_rate) ** (1 / self.train_epochs)
         steps_per_epoch = int(len(self.train_files) / self.batch_size)
