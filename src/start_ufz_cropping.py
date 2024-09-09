1854c516e83337f25c5fa8c05d88e239e6fd9f44
Marvin Müller <marvin.mueller@singular-it.de>
2024-09-09 12:28:12 +0200

    feat(evaluation): update evaluation methods

--- src/start_ufz_cropping.py
+++ src/start_ufz_cropping.py
@@ -40,19 +40,44 @@ def crop_raster(raster, shape, mem_raster=False, save=False, output_dir='', save
     return out_img, out_meta
 
 
-FILE_PATH = os.getcwd() + '/../20230612_leipzig-auwald-s_final-mosaic/leipzig-auwald-sued_20230612_ref_geo_mosaic.bsq'
-# HEADER_PATH = '../../data/UFZ_flightdata/leipzig-auwald-sued_20230612_ref_geo_mosaic.hdr'
+def resample_raster(raster_name, raster, resample_size, save_name, output_dir):
+    out_img = raster.read(
+        out_shape=(raster.count,
+                   resample_size[0],
+                   resample_size[1]),
+        resampling=rasterio.enums.Resampling.bilinear
+    )
+    # scale image transform
+    out_transform = raster.transform * raster.transform.scale(
+        (raster.width / out_img.shape[-1]),
+        (raster.height / out_img.shape[-2])
+    )
+    out_meta = raster.meta.copy()
+    out_meta.update({"driver": "GTiff",
+                     "height": out_img.shape[1],
+                     "width": out_img.shape[2],
+                     "transform": out_transform,
+                     })
+    with rasterio.open(output_dir + raster_name.strip('.tif') + save_name, "w",
+                       **out_meta) as dest:
+        dest.write(out_img)
 
-raster = rasterio.open(FILE_PATH)
+# FILE_PATH = os.getcwd() + '/../20230612_leipzig-auwald-s_final-mosaic/leipzig-auwald-sued_20230612_ref_geo_mosaic.bsq'
+# # HEADER_PATH = '../../data/UFZ_flightdata/leipzig-auwald-sued_20230612_ref_geo_mosaic.hdr'
+#
+# raster = rasterio.open(FILE_PATH)
+#
+# margin_y = 0
+# margin_x = 0
+# ul = raster.bounds[0] - margin_x, raster.bounds[3] + margin_y
+# ur = raster.bounds[2] - margin_x, raster.bounds[3] + margin_y
+# lr = raster.bounds[2] - margin_x, raster.bounds[1] + margin_y
+# ll = raster.bounds[0] - margin_x, raster.bounds[1] + margin_y
+# bbox = [[ul[0], ur[1]], [lr[0], ur[1]], [lr[0], ll[1]], [ul[0], ll[1]], [ul[0], ur[1]]]
+# crop_shape = [{'type': 'Polygon',
+#                'coordinates': [bbox]}]
+# UFZ_path = os.getcwd() + '/data/UFZ_flightdata/'
+# cropped_raster, out_meta = crop_raster(raster, crop_shape, mem_raster=True, save=True, output_dir=UFZ_path, save_name='UFZ_cropped_raster')
 
-margin_y = 0
-margin_x = 0
-ul = raster.bounds[0] - margin_x, raster.bounds[3] + margin_y
-ur = raster.bounds[2] - margin_x, raster.bounds[3] + margin_y
-lr = raster.bounds[2] - margin_x, raster.bounds[1] + margin_y
-ll = raster.bounds[0] - margin_x, raster.bounds[1] + margin_y
-bbox = [[ul[0], ur[1]], [lr[0], ur[1]], [lr[0], ll[1]], [ul[0], ll[1]], [ul[0], ur[1]]]
-crop_shape = [{'type': 'Polygon',
-               'coordinates': [bbox]}]
-UFZ_path = os.getcwd() + '/data/UFZ_flightdata/'
-cropped_raster, out_meta = crop_raster(raster, crop_shape, mem_raster=True, save=True, output_dir=UFZ_path, save_name='UFZ_cropped_raster')
+UFZ_cropped = os.getcwd() + '/data/UFZ_flightdata/UFZ_cropped_raster.tif'
+resample_raster('UFZ_cropped_raster.tif', rasterio.open(UFZ_cropped), (287, 210), '_resampled.tif', os.getcwd() + '/data/UFZ_flightdata/')
